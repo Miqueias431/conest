b4ad4ea05c61a19317df6cec1067cbf19cfb0e8c
Miqueias Rodrigues Barreto <miqueiasr560@gmail.com>
2024-06-19 16:12:45 -0300

    add

--- preload.js
+++ preload.js
@@ -1,7 +1,25 @@
-const { ipcRenderer } = require('electron')
+const {contextBridge, ipcRenderer } = require('electron')
 
 // Status de conexão (verifica se os bancos de dados está conectado)
 ipcRenderer.send('send-message', "Status do banco de dados:")
 ipcRenderer.on('db-status', (event, status) => {
     console.log(status)
+})
+
+// Inserir data na pagina
+function obterData() {
+    const data = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+
+    return data.toLocaleDateString('pt-br', options)
+}
+
+// Interagir diretamente no DOM do documento html (index.html)
+window.addEventListener('DOMContentLoaded', () =>{
+    const dataAtual = document.getElementById('dataAtual').innerHTML = obterData()
 })
