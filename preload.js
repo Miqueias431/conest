848d936b13021cbd908882fe6e1c96b1d3bc412d
Miqueias Rodrigues Barreto <miqueiasr560@gmail.com>
2024-08-19 14:26:38 -0300

    Feature: ...

--- preload.js
+++ preload.js
@@ -16,22 +16,31 @@ contextBridge.exposeInMainWorld('api', {
     openfornecedoresWindow: () => ipcRenderer.send('open-fornecedores-window'),
     openRelatorio: () => ipcRenderer.send('opne-relatorio'),
     dbMessage: (message) => ipcRenderer.on('db-message', message),
+    // Criação do cliente e fornecedor
     newClient: (cliente) => ipcRenderer.send('new-client', cliente),
     newFornecedor: (fornecedor) => ipcRenderer.send('new-fornecedor', fornecedor),
+    // Caixa para força a busca dos clientes ou fornecedores
     infoSearchDialog: () => ipcRenderer.send('dialog-infoSearchDialog'),
+    // Foco na caixa de busca
     focusSearch: (args) => ipcRenderer.on('focus-search', args),
+    // Busca do cliente e fornecedor
     searchClient: (nomeCliente) => ipcRenderer.send('search-client', nomeCliente),
     searchFornecedor: (nomeFornecedor) => ipcRenderer.send('search-fornecedor', nomeFornecedor),
     nameClient: (args) => ipcRenderer.on('name-cliente', args),
     nameFornecedor: (args) => ipcRenderer.on('name-fornecedor', args),
-    clearSearch: (args) => ipcRenderer.on('clear-search', args),
     dataClient: (dadosCliente) => ipcRenderer.on('data-client',dadosCliente),
     dataFornecedor: (dadosFornecedor) => ipcRenderer.on('data-fornecedor',dadosFornecedor),
+    // Limpar a caixa de busca
+    clearSearch: (args) => ipcRenderer.on('clear-search', args),
+    // Resetar o formulaário 
     resetForm: (args) => ipcRenderer.on('reset-form', args),
+    // Atualizar o cliente e fornecedor
     updateClient: (cliente) => ipcRenderer.send('update-client', cliente),
     updateFornecedor: (fornecedor) => ipcRenderer.send('update-fornecedor', fornecedor),
+    // Deletar o cliente e fornecedor
     deleteClient: (idCli) => ipcRenderer.send('delete-client', idCli),
     deleteFornecedor: (idForn) => ipcRenderer.send('delete-fornecedor', idForn),
+    // Limpar todos os clientes e fornecedores
     clearClient:(clearCliente) => ipcRenderer.on('clear-all-client', clearCliente),
     focusClient:(focusCliente) => ipcRenderer.on('focus-client', focusCliente)
 })
